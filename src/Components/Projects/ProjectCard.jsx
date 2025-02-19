d00f8f8e7106d27bf4be463ea8add1b2c95e62cf
jimenezandres07 <75646537+jimenezandres07@users.noreply.github.com>
2025-02-19 16:52:37 -0300

    photos fixed

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -13,7 +13,7 @@ const ProjectCard = ({
 		<div className="project__card">
 			<h2>{projectName}</h2>
 			<div className="project__image">
-				<img src={projectImage} alt={projectName} />
+				<img src={projectImage} alt={projectName} className="project__image_photo" />
 			</div>
 			<h5>{projectDescription}</h5>
 			<CTAProjects githubLink={githubLink} deployLink={deployLink} />
